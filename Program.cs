298d4dccda3e5670542d312e6a841655a3b23c48
Techjonlogi <61212739+Techjonlogi@users.noreply.github.com>
2020-05-19 14:37:21 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,26 +1,16 @@
-﻿// Proxy Design Pattern
-//
-// Intent: Lets you provide a substitute or placeholder for another object. A
-// proxy controls access to the original object, allowing you to perform
-// something either before or after the request gets through to the original
-// object.
+
 
 using System;
 
 namespace RefactoringGuru.DesignPatterns.Proxy.Conceptual
 {
-    // The Subject interface declares common operations for both RealSubject and
-    // the Proxy. As long as the client works with RealSubject using this
-    // interface, you'll be able to pass it a proxy instead of a real subject.
+   
     public interface ISubject
     {
         void Request();
     }
     
-    // The RealSubject contains some core business logic. Usually, RealSubjects
-    // are capable of doing some useful work which may also be very slow or
-    // sensitive - e.g. correcting input data. A Proxy can solve these issues
-    // without any changes to the RealSubject's code.
+  
     class RealSubject : ISubject
     {
         public void Request()
@@ -29,7 +19,7 @@ namespace RefactoringGuru.DesignPatterns.Proxy.Conceptual
         }
     }
     
-    // The Proxy has an interface identical to the RealSubject.
+    
     class Proxy : ISubject
     {
         private RealSubject _realSubject;
@@ -39,10 +29,7 @@ namespace RefactoringGuru.DesignPatterns.Proxy.Conceptual
             this._realSubject = realSubject;
         }
         
-        // The most common applications of the Proxy pattern are lazy loading,
-        // caching, controlling the access, logging, etc. A Proxy can perform
-        // one of these things and then, depending on the result, pass the
-        // execution to the same method in a linked RealSubject object.
+        
         public void Request()
         {
             if (this.CheckAccess())
@@ -56,7 +43,7 @@ namespace RefactoringGuru.DesignPatterns.Proxy.Conceptual
 		
         public bool CheckAccess()
         {
-            // Some real checks should go here.
+            
             Console.WriteLine("Proxy: Checking access prior to firing a real request.");
 
             return true;
@@ -70,11 +57,7 @@ namespace RefactoringGuru.DesignPatterns.Proxy.Conceptual
     
     public class Client
     {
-        // The client code is supposed to work with all objects (both subjects
-        // and proxies) via the Subject interface in order to support both real
-        // subjects and proxies. In real life, however, clients mostly work with
-        // their real subjects directly. In this case, to implement the pattern
-        // more easily, you can extend your proxy from the real subject's class.
+        
         public void ClientCode(ISubject subject)
         {
             // ...
